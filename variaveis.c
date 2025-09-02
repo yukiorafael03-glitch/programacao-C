fa8878dc1a2007bd212d8640e2da0b07fabc5b54
RAFAEL YUKIO SATO <yukiorafael03@gmail.com>
2025-09-02 20:29:25 -0300

    Initial commit

---
+++ variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 35;
+    int quantidade = 1;
+    float altura = 1,75;
+    double peso = 76;
+    char letra = 'R'
+    char nome[20] = "rafael"; 
+
+    printf("A idade do %s é: %d\nnome, idade);
+    printf("A altura é: %e", altura);
+    printf("A opcao %c", opcao);
+
+
+
+
+
+
+}
